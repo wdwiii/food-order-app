a733917463db1102704afb2a0bb5e581451eb3cb
wdwiii <wdwhitfield3@gmail.com>
2022-10-13 19:44:47 -0400

    Delete css module and use makeStyles hook

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,35 +1,57 @@
-import { useContext } from 'react';
+import { useContext } from 'react'
+import MealItemForm from './MealItemForm'
+import CartContext from '../../../store/cart-context'
+import { Typography, MenuItem } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
-import CartContext from '../../../store/cart-context';
+const useStyles = makeStyles({
+  meal: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '1rem',
+    paddingBottom: '1rem',
+    borderBottom: '1px solid #ccc',
+  },
+  description: { fontStyle: 'italic' },
+  price: {
+    marginTop: '0.25rem',
+    fontWeight: 'bold',
+    color: '#ad5502',
+    fontSize: '1.25rem',
+  },
+  name: {
+    margin: '0 0 0.25rem 0',
+  },
+})
 
-const MealItem = (props) => {
-  const cartCtx = useContext(CartContext);
+const MealItem = ({ id, name, description, price }) => {
+  const classes = useStyles()
+  const cartCtx = useContext(CartContext)
+  const formattedPrice = `$${price.toFixed(2)}`
 
-  const price = `$${props.price.toFixed(2)}`;
-
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
-      id: props.id,
-      name: props.name,
+      id: id,
+      name: name,
       amount: amount,
-      price: props.price
-    });
-  };
+      price: formattedPrice,
+    })
+  }
 
   return (
-    <li className={classes.meal}>
+    <MenuItem className={classes.meal}>
       <div>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <Typography variant='h5' className={classes.name}>
+          {name}
+        </Typography>
+        <Typography className={classes.description}>{description}</Typography>
+        <Typography className={classes.price}>{formattedPrice}</Typography>
       </div>
       <div>
         <MealItemForm onAddToCart={addToCartHandler} />
       </div>
-    </li>
-  );
-};
+    </MenuItem>
+  )
+}
 
-export default MealItem;
+export default MealItem
