142511645d58464502a9c664235c338be6186ddc
wdwiii <wdwhitfield3@gmail.com>
2022-10-14 21:47:15 -0400

    Add error handling for meal data fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import MealItem from './MealItem/MealItem'
 import classes from './AvailableMeals.module.css'
-import { Backdrop, CircularProgress, Card } from '@mui/material'
+import {
+  Backdrop,
+  CircularProgress,
+  Card,
+  List,
+  Typography,
+} from '@mui/material'
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     const fetchMeals = async () => {
-      try {
-        const response = await fetch(
-          'https://react-http-af1c7-default-rtdb.firebaseio.com/meals.json'
-        )
-        const data = await response.json()
-
-        const mealsArray = []
-        for (let key in data) {
-          mealsArray.push({
-            id: key,
-            name: data[key].name,
-            description: data[key].description,
-            price: data[key].price,
-          })
-        }
-        setMeals(mealsArray)
-        console.log('🚀 ~ fetchMeals ~ mealsArray', mealsArray)
-      } catch (error) {
-        console.error('error')
-      } finally {
-        setLoading(false)
+      const response = await fetch(
+        'https://react-http-af1c7-default-rtdb.firebaseio.com/meals.json'
+      )
+      console.log('🚀 ~ fetchMeals ~ response', response)
+      if (!response.ok) {
+        throw new Error('Something went wrong')
       }
+      const data = await response.json()
+
+      const mealsArray = []
+      for (let key in data) {
+        mealsArray.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        })
+      }
+      setMeals(mealsArray)
+      setLoading(false)
+      console.log('🚀 ~ fetchMeals ~ mealsArray', mealsArray)
     }
 
-    fetchMeals()
+    fetchMeals().catch((error) => {
+      console.error(error.message)
+      setLoading(false)
+      setError(true)
+    })
   }, [])
 
   const renderMealsList = () =>
@@ -53,8 +63,13 @@ const AvailableMeals = () => {
     </Backdrop>
   ) : (
     <section className={classes.meals}>
-      <Card>
-        <ul>{renderMealsList()}</ul>
+      <Card style={{ minHeight: 150 }}>
+        <List>{renderMealsList()}</List>
+        {error ? (
+          <Typography align='center'>
+            Uh oh, something isn't right...
+          </Typography>
+        ) : null}
       </Card>
     </section>
   )
